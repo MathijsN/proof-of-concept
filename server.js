fd2577391075834919b6720f9e194e5e3a8d5122
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-06-09 12:52:08 +0200

    feat: anchor jump link #8

--- server.js
+++ server.js
@@ -2,19 +2,14 @@ import express, { response } from 'express'
 import { Liquid } from 'liquidjs';
 
 const app = express()
-
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.static('public'))
-
 const engine = new Liquid()
 app.engine('liquid', engine.express())
-
 app.set('views', './views')
 
-// Gebruikers id Anne-Fleur Pietersen
 
-app.get('/', async function (request, response) {
+app.get('/', async function (req, res) {
 
     const sectionsApiResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections')
     const sectionsApiResponseJSON = await sectionsApiResponse.json()
@@ -24,9 +19,28 @@ app.get('/', async function (request, response) {
     const eexhibitApiResponseJSON = await exhibitApiResponse.json()
     const exhibit = eexhibitApiResponseJSON.data
 
-    response.render('index.liquid', { sectionsList, exhibit })
+    const quizApiResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_quiz_questions')
+    const quizApiResponseJSON = await quizApiResponse.json()
+    const quiz = quizApiResponseJSON.data
+
+    // console.log(quiz)
+    console.log(sectionsList)
+    res.render('index.liquid', { sectionsList, exhibit, quiz })
 })
 
+
+app.get('/detail/:slug', async function (req, res) {
+    const params = new URLSearchParams()
+    params.set('filter[slug][_eq]', req.params.slug)
+
+    const sectionsApiResponse = await fetch('https://fdnd-agency.directus.app/items/teylers_museum_exhibits_sections?fields=*.*.*&filter[slug][_eq]=' + req.params.slug)
+    const sectionsApiResponseJSON = await sectionsApiResponse.json()
+    const sectionsList = sectionsApiResponseJSON.data
+
+    console.log(sectionsList)
+
+    res.render('detail.liquid', { section: sectionsList[0] })
+})
 
 app.use((req, res) => {
     res.status(404).render('404.liquid')
